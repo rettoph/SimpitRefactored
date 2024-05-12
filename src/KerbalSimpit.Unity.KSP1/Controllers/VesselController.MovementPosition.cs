24f8c3abd537daaeab7d6317a18646196aa5e688
rettoph <anthony@turner.ec>
2024-05-11 22:33:04 -0600

    Minor code cleanup

--- src/KerbalSimpit.Unity.KSP1/Controllers/VesselController.MovementPosition.cs
+++ src/KerbalSimpit.Unity.KSP1/Controllers/VesselController.MovementPosition.cs
@@ -25,6 +25,84 @@ namespace KerbalSimpit.Unity.KSP1.Controllers
 
         public FlightCtrlState LastFlightCtrlState => _lastFlightCtrlState;
 
+        private void AutopilotUpdater(FlightCtrlState fcs)
+        {
+            // For all axis, we need to control both the usual axis and make sure the matching custom axis are also controlled.
+            // This does not seem possible in 1.12.2 without using 2 different methods to set the axis values.
+            // If any change is made to the following code, please check both (e.g. add a piston to the roll axis and check that setting roll also control the piston).
+            var axisGroupModule = _vessel.FindVesselModuleImplementing<AxisGroupsModule>();
+
+            if (_rotation.Pitch != 0)
+            {
+                fcs.pitch = (float)_rotation.Pitch / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Pitch, (float)_rotation.Pitch / Int16.MaxValue);
+            }
+            if (_rotation.Roll != 0)
+            {
+                fcs.roll = (float)_rotation.Roll / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Roll, (float)_rotation.Roll / Int16.MaxValue);
+            }
+            if (_rotation.Yaw != 0)
+            {
+                fcs.yaw = (float)_rotation.Yaw / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Yaw, (float)_rotation.Yaw / Int16.MaxValue);
+            }
+
+            if (_translation.X != 0)
+            {
+                fcs.X = (float)_translation.X / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.TranslateX, (float)_translation.X / Int16.MaxValue);
+            }
+            if (_translation.Y != 0)
+            {
+                fcs.Y = (float)_translation.Y / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.TranslateY, (float)_translation.Y / Int16.MaxValue);
+            }
+            if (_translation.Z != 0)
+            {
+                fcs.Z = (float)_translation.Z / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.TranslateZ, (float)_translation.Z / Int16.MaxValue);
+            }
+
+            if (_wheel.Steer != 0)
+            {
+                fcs.wheelSteer = (float)_wheel.Steer / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.WheelSteer, (float)_wheel.Steer / Int16.MaxValue);
+            }
+            if (_wheel.Throttle != 0)
+            {
+                fcs.wheelThrottle = (float)_wheel.Throttle / Int16.MaxValue;
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.WheelThrottle, (float)_wheel.Throttle / Int16.MaxValue);
+            }
+
+            if (_throttle.Value != 0 || !_lastThrottleSentIsZero)
+            {
+                // Throttle seems to be handled differently than the other axis since when no value is set, a zero is assumed. For throttle, no value set mean the last one is used.
+                // So we need to send a 0 first before stopping to send values.
+                FlightInputHandler.state.mainThrottle = (float)_throttle.Value / Int16.MaxValue;
+
+                _lastThrottleSentIsZero = (_throttle.Value == 0);
+            }
+            if (_custom.Custom1 != 0)
+            {
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom01, (float)_custom.Custom1 / Int16.MaxValue);
+            }
+            if (_custom.Custom2 != 0)
+            {
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom02, (float)_custom.Custom2 / Int16.MaxValue);
+            }
+            if (_custom.Custom3 != 0)
+            {
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom03, (float)_custom.Custom3 / Int16.MaxValue);
+            }
+            if (_custom.Custom4 != 0)
+            {
+                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom04, (float)_custom.Custom4 / Int16.MaxValue);
+            }
+
+            // Store the last flight command to send them in the dedicated channels
+            _lastFlightCtrlState.CopyFrom(fcs);
+        }
 
 
         public void Process(SimpitPeer peer, ISimpitMessage<VesselMessages.Incoming.Rotation> message)
@@ -132,85 +210,6 @@ namespace KerbalSimpit.Unity.KSP1.Controllers
         private void OnPostAutopilotUpdateHandler(FlightCtrlState st)
         {
             this.AutopilotUpdater(st);
-        }
-
-        private void AutopilotUpdater(FlightCtrlState fcs)
-        {
-            // For all axis, we need to control both the usual axis and make sure the matching custom axis are also controlled.
-            // This does not seem possible in 1.12.2 without using 2 different methods to set the axis values.
-            // If any change is made to the following code, please check both (e.g. add a piston to the roll axis and check that setting roll also control the piston).
-            var axisGroupModule = _vessel.FindVesselModuleImplementing<AxisGroupsModule>();
-
-            if (_rotation.Pitch != 0)
-            {
-                fcs.pitch = (float)_rotation.Pitch / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Pitch, (float)_rotation.Pitch / Int16.MaxValue);
-            }
-            if (_rotation.Roll != 0)
-            {
-                fcs.roll = (float)_rotation.Roll / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Roll, (float)_rotation.Roll / Int16.MaxValue);
-            }
-            if (_rotation.Yaw != 0)
-            {
-                fcs.yaw = (float)_rotation.Yaw / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Yaw, (float)_rotation.Yaw / Int16.MaxValue);
-            }
-
-            if (_translation.X != 0)
-            {
-                fcs.X = (float)_translation.X / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.TranslateX, (float)_translation.X / Int16.MaxValue);
-            }
-            if (_translation.Y != 0)
-            {
-                fcs.Y = (float)_translation.Y / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.TranslateY, (float)_translation.Y / Int16.MaxValue);
-            }
-            if (_translation.Z != 0)
-            {
-                fcs.Z = (float)_translation.Z / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.TranslateZ, (float)_translation.Z / Int16.MaxValue);
-            }
-
-            if (_wheel.Steer != 0)
-            {
-                fcs.wheelSteer = (float)_wheel.Steer / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.WheelSteer, (float)_wheel.Steer / Int16.MaxValue);
-            }
-            if (_wheel.Throttle != 0)
-            {
-                fcs.wheelThrottle = (float)_wheel.Throttle / Int16.MaxValue;
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.WheelThrottle, (float)_wheel.Throttle / Int16.MaxValue);
-            }
-
-            if (_throttle.Value != 0 || !_lastThrottleSentIsZero)
-            {
-                // Throttle seems to be handled differently than the other axis since when no value is set, a zero is assumed. For throttle, no value set mean the last one is used.
-                // So we need to send a 0 first before stopping to send values.
-                FlightInputHandler.state.mainThrottle = (float)_throttle.Value / Int16.MaxValue;
-
-                _lastThrottleSentIsZero = (_throttle.Value == 0);
-            }
-            if (_custom.Custom1 != 0)
-            {
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom01, (float)_custom.Custom1 / Int16.MaxValue);
-            }
-            if (_custom.Custom2 != 0)
-            {
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom02, (float)_custom.Custom2 / Int16.MaxValue);
-            }
-            if (_custom.Custom3 != 0)
-            {
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom03, (float)_custom.Custom3 / Int16.MaxValue);
-            }
-            if (_custom.Custom4 != 0)
-            {
-                axisGroupModule.UpdateAxisGroup(KSPAxisGroup.Custom04, (float)_custom.Custom4 / Int16.MaxValue);
-            }
-
-            // Store the last flight command to send them in the dedicated channels
-            _lastFlightCtrlState.CopyFrom(fcs);
         }
     }
 }
--- tests/KerbalSimpit.Test.Core/COBSHelperTest.cs
+++ tests/KerbalSimpit.Test.Core/COBSHelperTest.cs
@@ -1,7 +1,6 @@
 ﻿using KerbalSimpit.Core;
 using KerbalSimpit.Core.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using System.Linq;
 
 namespace KerbalSimpit.Test.Core
@@ -10,10 +9,10 @@ namespace KerbalSimpit.Test.Core
     public class COBSHelperTest
     {
         [TestMethod]
-        [DataRow(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },       DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_01")]
-        [DataRow(new byte[] { 0, 1, 0, 1, 0, 1, 0, 1 },          DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_02")]
-        [DataRow(new byte[] { 0, 1, 2, 4, 8, 16, 32, 64, 128 },  DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_03")]
-        [DataRow(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 },       DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_04")]
+        [DataRow(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_01")]
+        [DataRow(new byte[] { 0, 1, 0, 1, 0, 1, 0, 1 }, DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_02")]
+        [DataRow(new byte[] { 0, 1, 2, 4, 8, 16, 32, 64, 128 }, DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_03")]
+        [DataRow(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, DisplayName = "COBSHelper_Decode_Matches_Encode_Scenario_04")]
         public void COBSHelper_Decode_Matches_Encode(byte[] data)
         {
             SimpitStream input = new SimpitStream();
@@ -33,7 +32,7 @@ namespace KerbalSimpit.Test.Core
             byte[] decoded = output.ReadAll(out offset, out count).Skip(offset).Take(count).ToArray();
 
             Assert.AreEqual(data.Length, decoded.Length);
-            for(int i=0; i<data.Length; i++)
+            for (int i = 0; i < data.Length; i++)
             {
                 Assert.AreEqual(data[i], decoded[i]);
             }
