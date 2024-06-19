56b89342ed6a5af38a51435e072ad9cca0afb8ca
rettoph <anthony@turner.ec>
2024-06-19 17:51:58 -0600

    Add connection recovery to proxy project

--- src/KerbalSimpit.Proxy.SerialThroughTCP/Program.cs
+++ src/KerbalSimpit.Proxy.SerialThroughTCP/Program.cs
@@ -4,4 +4,6 @@ using System.Text.Json;
 SimpitProxyConfiguration[] configuration = JsonSerializer.Deserialize<SimpitProxyConfiguration[]>(File.ReadAllText("simpit.proxy.config.json")) ?? Array.Empty<SimpitProxyConfiguration>();
 SimpitProxy[] proxies = configuration.Select(x => new SimpitProxy(x)).ToArray();
 
-await Task.WhenAll(proxies.Select(x => x.RunAsync()));
+await Task.WhenAll(proxies.Select(x => x.StartAsync()));
+
+Console.ReadLine();
